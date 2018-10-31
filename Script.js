5e08f80577f1ae824369ebbceb133f154dfa1945
Brian Shisanya <34907795+Minotauro49@users.noreply.github.com>
2018-10-31 23:44:51 +1100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+
+// 	$(this).mousemove(function(){
+// 		$("circle").css("cx",$("#range").val())
+// 		$("circle").css("cy",$("#range2").val())
+// 		// console.log();
+
+// 	})
+
+// })
+
+
+
+
